e024f443f84ade4b56d09fb41042a484ea9e5b7a
Batsatsugo <corajianchristian@gmail.com>
2025-03-13 15:27:18 +0800

    while loop and if and else conditions and try and except value error

--- input_a_number_print_invalid_or_unique_or_duplicate.py
+++ input_a_number_print_invalid_or_unique_or_duplicate.py
@@ -1,4 +1,7 @@
 # DECLARE number_list, number,
+
+number_list = []
+
 # WHILE TRUE
 # TRY
 # number = ('Enter a number: ')
@@ -9,3 +12,13 @@
 # number_list.append(number)
 # EXCEPT ValueError
 # PRINT('invalid')
+while True:
+    try:
+        number = int(input("Enter a number: "))
+        if number in numberList:
+            print("Duplicate")
+        else:
+            print("Unique")
+            numberList.append(number)
+    except ValueError:
+        print('invalid')
