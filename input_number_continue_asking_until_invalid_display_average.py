ee1e84db7e0a95724f0ca5ba8f3acd727f8cfcd2
Batsatsugo <corajianchristian@gmail.com>
2025-03-13 19:44:19 +0800

    pseudo code added

---
+++ input_number_continue_asking_until_invalid_display_average.py
@@ -0,0 +1,11 @@
+# DECLARE count, sum, num, average
+# WHILE TRUE
+# TRY
+# num = ('Enter a number (or any non-numeric value to stop): ')
+# sum += num
+# count += 1
+# EXCEPT ValueError
+# BREAK
+# IF count > 0
+# average = sum / count
+# PRINT('Average: {average}')
