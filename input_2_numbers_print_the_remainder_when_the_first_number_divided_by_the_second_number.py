4d6d0a251dc914db4bac12fac6c8d931d482f2bb
Batsatsugo <corajianchristian@gmail.com>
2025-03-13 13:46:12 +0800

    file created

--- input_2_numbers_print_the_remainder_when_the_first_number_divided_by_the_second_number.py
+++ input_2_numbers_print_the_remainder_when_the_first_number_divided_by_the_second_number.py
@@ -1,5 +1,13 @@
 # DECLARE first_number, second_number,remainder
+
 # INPUT first_number
+first_number = int(input('Enter first number: '))
+
 # INPUT second_number
+second_number = int(input('Enter first number: '))
+
 # remainder = first_number % second_number
+remainder = first_number % second_number
+
 # PRINT (remainder)
+print(remainder)
