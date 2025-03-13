8d72586805f86c6c669788b6f0593e5fdb8dd194
Batsatsugo <corajianchristian@gmail.com>
2025-03-13 15:32:51 +0800

    pseudo code added

---
+++ input_a_number_and_stop_when_input_is_invalid_and_display_the_lowest_number.py
@@ -0,0 +1,8 @@
+# DECLARE number_list, number
+# WHILE TRUE
+# TRY
+# number =('Enter a number: ')
+# number_list.append(number)
+# EXCEPT Valueerror
+# BREAK
+# PRINT('Lowest number: ', min(number_list))
