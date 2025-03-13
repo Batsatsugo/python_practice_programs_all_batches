050db1520af943c5afb1ac69df714dd70735ec05
Batsatsugo <corajianchristian@gmail.com>
2025-03-13 13:51:51 +0800

    if condition

--- ask_user_10_numbers_print_the_result_of_the_first_number_minus_all_of_the_remaining_numbers.py
+++ ask_user_10_numbers_print_the_result_of_the_first_number_minus_all_of_the_remaining_numbers.py
@@ -1,7 +1,14 @@
 # DECLARE numbers, num, sum_remaining, result
+
+numbers = []
+
 # FOR i in RANGE (10)
 # num = ('Enter number {i + 1} ')
 # numbers.append(num)
+for i in range(10):
+    num = float(input(f"Enter number {i + 1}: "))
+    numbers.append(num)
+
 # sum_remaining = sum(numbers[1:])
 # result = numbers[0] - sum_remaining
 # PRINT("Result:", result)
