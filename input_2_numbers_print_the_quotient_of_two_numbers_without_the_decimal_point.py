f828e2606eb6af8c2cf463f2b1daccef6ab2e35e
Batsatsugo <corajianchristian@gmail.com>
2025-03-13 13:28:41 +0800

    pseudocode created

---
+++ input_2_numbers_print_the_quotient_of_two_numbers_without_the_decimal_point.py
@@ -0,0 +1,8 @@
+# DECLARE first_number, second_number, quotient
+# INPUT first_number
+# INPUT second_number
+# IF second_number not equal to 0
+# quotient = first_number // second_number
+# PRINT ("Quotient: ", quotient)
+# ELSE
+# PRINT ("Error: Division by zero is not allowed")
