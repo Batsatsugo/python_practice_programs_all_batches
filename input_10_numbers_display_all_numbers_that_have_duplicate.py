f65030ee3dc1cca4130810c5fc5d84088152bdbe
Batsatsugo <corajianchristian@gmail.com>
2025-03-13 18:37:19 +0800

    pseudocode added

---
+++ input_10_numbers_display_all_numbers_that_have_duplicate.py
@@ -0,0 +1,6 @@
+# DECLARE number_list, number, duplicate
+# FOR i IN RANGE(1, 11)
+# number = ('Enter a number')
+# number_list.append(number)
+# duplicate = [number for number in number_list if number_list.append(number) == 1]
+# PRINT(duplicate)
