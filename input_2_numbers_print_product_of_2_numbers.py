8611e989c96100b1c65d3487f9e97490e1eb2535
Batsatsugo <corajianchristian@gmail.com>
2025-03-13 12:07:09 +0800

    print product of the two numbers

--- input_2_numbers_print_product_of_2_numbers.py
+++ input_2_numbers_print_product_of_2_numbers.py
@@ -1,5 +1,13 @@
 # DECLARE first_number, second_number, product
+
 # INPUT first_number
+first_number = int(input('Enter first number: '))
+
 # INPUT second_number
+second_number = int(input('Enter second number: '))
+
 # product = first_number + second_number
+product = first_number + second_number
+
 # PRINT (The sum of the two numbers is " + product
+print("The sum of the two numbers is " + str(product))
