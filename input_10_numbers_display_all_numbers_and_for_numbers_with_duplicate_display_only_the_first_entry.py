82e73d03e4a914270dd0363421d7338b1983561e
Batsatsugo <corajianchristian@gmail.com>
2025-03-13 15:12:47 +0800

    for loop and if condition

--- input_10_numbers_display_all_numbers_and_for_numbers_with_duplicate_display_only_the_first_entry.py
+++ input_10_numbers_display_all_numbers_and_for_numbers_with_duplicate_display_only_the_first_entry.py
@@ -1,6 +1,14 @@
 # DECLARE number_list, number,
+
+number_list = []
+
 # FOR i IN RANGE(1, 11)
 # number = ('Enter a number')
 # IF number NOT IN number_list
 # number_list.append(number)
+for i in range(1, 11):
+    number = int(input("Enter a number: "))
+    if number not in numberList:
+        numberList.append(number)
+
 # PRINT(number_list)
