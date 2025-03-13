b8b3b4e143b9382be45df0fa50fdb601fdc010e4
Batsatsugo <corajianchristian@gmail.com>
2025-03-13 12:18:36 +0800

    created pseudocode and the print the result

---
+++ input_2_numbers_print_the_quotient_of_two_numbers_with_decimal_point.py
@@ -0,0 +1,13 @@
+# DECLARE first_number, second_number, quotient
+
+# INPUT first_number
+first_number = int(input('Enter first number: '))
+
+# INPUT second_number
+second_number = int(input('Enter second number: '))
+
+# quotient = first_number + second_number
+quotient = first_number / second_number
+
+# PRINT (The sum of the two numbers is " + quotient
+print('The quotient of the two numbers is ' + str(quotient))
