6a21c37d7405c22a427580753d4a33da99598561
Batsatsugo <corajianchristian@gmail.com>
2025-03-13 15:08:28 +0800

    print non duplicated

--- input_10_numbers_display_all_numbers_that_dont_have_duplicate.py
+++ input_10_numbers_display_all_numbers_that_dont_have_duplicate.py
@@ -1,6 +1,15 @@
-# DECLARE number_list, number
+# DECLARE number_list, number, non_duplicated
+
+number_list = []
 # FOR i IN RANGE(1, 11)
 # number = ('Enter a number')
-# IF number NOT IN number_list
 # number_list.append(number)
-# PRINT(number_list)
+for i in range(1, 11):
+    number = int(input("Enter a number: "))
+    number_list.append(number)
+
+# non_duplicated = [number for number in number_list if number_list.count(number) == 1
+non_duplicated = [number for number in number_list if number_list.count(number) == 1]
+
+# PRINT(non_duplicated)
+print(non_duplicated)
