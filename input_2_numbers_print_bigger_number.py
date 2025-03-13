a08b214bba9f387032800e6c3fea95473c55c87a
Batsatsugo <corajianchristian@gmail.com>
2025-03-13 11:34:00 +0800

    changed sign (correct sign)

--- input_2_numbers_print_bigger_number.py
+++ input_2_numbers_print_bigger_number.py
@@ -5,9 +5,9 @@ first_number = int(input('Enter first number: '))
 # INPUT second_number
 second_number = int(input('Enter second number: '))
 
-# IF first_number < second_number
+# IF first_number > second_number
 # PRINT first_number
-if first_number < second_number:
+if first_number > second_number:
     print(first_number)
 
 # ELSE
