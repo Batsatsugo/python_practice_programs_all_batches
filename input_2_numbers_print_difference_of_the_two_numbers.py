4387666dd6d7e3cfe2aa6d49fd6f46cbf92b04dc
Batsatsugo <corajianchristian@gmail.com>
2025-03-13 13:22:46 +0800

    print difference of thw two numbers

--- input_2_numbers_print_difference_of_the_two_numbers.py
+++ input_2_numbers_print_difference_of_the_two_numbers.py
@@ -1,5 +1,13 @@
 # DECLARE first_number, second_number, difference
+
 # INPUT first_number
+first_number = int(input('Enter first number: '))
+
 # INPUT second_number
+second_number = int(input('Enter first number: '))
+
 # difference = first_number - second_number
+difference = first_number - second_number
+
 # PRINT (The sum of the two numbers is " + difference)
+print(f'The difference of the two numbers is: {difference}')
