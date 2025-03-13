6656e514342051c4b0d3c40ffedcc51a396c4eab
Batsatsugo <corajianchristian@gmail.com>
2025-03-13 19:16:19 +0800

    while loop and try and except

--- input_a_number_continue_asking_until_invalid_diplay_number_with_most_number_of_duplicate.py
+++ input_a_number_continue_asking_until_invalid_diplay_number_with_most_number_of_duplicate.py
@@ -1,4 +1,7 @@
 # DECLARE numbers, num, most_repeated
+
+numbers = {}
+
 # WHILE TRUE
 # TRY
 # num =('Enter a number: ')
@@ -8,6 +11,17 @@
 # numbers[num] = 1
 # EXCEPT ValueError
 # BREAK
+while True:
+    try:
+        num = int(input("Enter a number: "))
+        if num in numbers:
+            numbers[num] += 1
+        else:
+            numbers[num] = 1
+    except ValueError:
+        break
+
+
 # IF numbers
 # most_repeated = max(numbers, key=numbers.get)
 # PRINT(f"The number with the most duplicate is: {most_repeated}")
