afcf65f0c646d499803bc263a87123f5fd2f4b26
Batsatsugo <corajianchristian@gmail.com>
2025-03-13 15:25:08 +0800

    pseudo code

---
+++ input_a_number_print_invalid_or_unique_or_duplicate.py
@@ -0,0 +1,11 @@
+# DECLARE number_list, number,
+# WHILE TRUE
+# TRY
+# number = ('Enter a number: ')
+# IF number in number_list
+# PRINT('duplicate')
+# ELSE
+# PRINT('unique')
+# number_list.append(number)
+# EXCEPT ValueError
+# PRINT('invalid')
