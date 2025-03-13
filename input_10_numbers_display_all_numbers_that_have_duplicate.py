132c6e6896e2ac883cb4b11b7aff5aefe80e37e9
Batsatsugo <corajianchristian@gmail.com>
2025-03-13 18:39:35 +0800

    for loop and append

--- input_10_numbers_display_all_numbers_that_have_duplicate.py
+++ input_10_numbers_display_all_numbers_that_have_duplicate.py
@@ -1,6 +1,13 @@
 # DECLARE number_list, number, duplicate
+
+number_list = []
+
 # FOR i IN RANGE(1, 11)
 # number = ('Enter a number')
 # number_list.append(number)
+for i in range(1, 11)
+    number = (int(input("Enter a number: ")))
+    number_list.append(number)
+    
 # duplicate = [number for number in number_list if number_list.append(number) == 1]
 # PRINT(duplicate)
