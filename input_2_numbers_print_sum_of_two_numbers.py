9ec8d85c782a6bf1b45102fc1aa072ac8b07986d
Batsatsugo <corajianchristian@gmail.com>
2025-03-13 11:58:18 +0800

    print the result or sum of the two numbers

--- input_2_numbers_print_sum_of_two_numbers.py
+++ input_2_numbers_print_sum_of_two_numbers.py
@@ -1,5 +1,13 @@
 # DECLARE first_number, second_number,sum
+
 # INPUT first_number
+first_number = int(input('Enter first number: '))
+
 # INPUT second_number
+second_number = int(input('Enter second number: '))
+
 # sum = first_number + second_number
+sum = first_number + second_number
+
 # PRINT (The sum of the two numbers is " + sum)
+print("The sum of the two numbers is " + str(sum))
