f8200ed04275f2f604be29f1158d3b4477f619ba
Batsatsugo <corajianchristian@gmail.com>
2025-03-13 15:35:29 +0800

    while loop and try and except value error

--- input_a_number_and_stop_when_input_is_invalid_and_display_the_lowest_number.py
+++ input_a_number_and_stop_when_input_is_invalid_and_display_the_lowest_number.py
@@ -1,8 +1,18 @@
 # DECLARE number_list, number
+
+numberList = []
+
 # WHILE TRUE
 # TRY
 # number =('Enter a number: ')
 # number_list.append(number)
 # EXCEPT Value error
 # BREAK
+while True:
+    try:
+        number = int(input("Enter a number: "))
+        numberList.append(number)
+    except ValueError:
+        break
+        
 # PRINT('Lowest number: ', min(number_list))
