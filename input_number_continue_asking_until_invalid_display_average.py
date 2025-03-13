fc7b2e1a1a02f6352c1226386ef10ecf7aa1e012
Batsatsugo <corajianchristian@gmail.com>
2025-03-13 19:45:02 +0800

    while loop and try and except

--- input_number_continue_asking_until_invalid_display_average.py
+++ input_number_continue_asking_until_invalid_display_average.py
@@ -1,4 +1,8 @@
 # DECLARE count, sum, num, average
+
+count = 0
+sum_numbers = 0
+
 # WHILE TRUE
 # TRY
 # num = ('Enter a number (or any non-numeric value to stop): ')
@@ -6,6 +10,14 @@
 # count += 1
 # EXCEPT ValueError
 # BREAK
+while True:
+    try:
+        num = float(input("Enter a number (or any non-numeric value to stop): "))
+        sum_numbers += num
+        count += 1
+    except ValueError:
+        break
+
 # IF count > 0
 # average = sum / count
 # PRINT('Average: {average}')
