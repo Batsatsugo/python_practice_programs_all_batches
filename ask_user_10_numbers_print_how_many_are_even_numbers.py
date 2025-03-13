5463cb201e834789df8f1cd1848b1dfee917c21f
Batsatsugo <corajianchristian@gmail.com>
2025-03-13 14:02:44 +0800

    for loop and if condition

--- ask_user_10_numbers_print_how_many_are_even_numbers.py
+++ ask_user_10_numbers_print_how_many_are_even_numbers.py
@@ -1,6 +1,14 @@
 # DECLARE even_numbers, num
+
+even_numbers = 0
+
 # FOR i IN RANGE (10)
 # num = (Enter number {i + 1} )
 # IF num % 2 == 0:
 # even_numbers += 1
+for i in range(10):
+    num = int(input(f"Enter number {i + 1}: "))
+    if num % 2 == 0:
+        even_numbers += 1
+
 # PRINT(Total even numbers: , even_numbers)
