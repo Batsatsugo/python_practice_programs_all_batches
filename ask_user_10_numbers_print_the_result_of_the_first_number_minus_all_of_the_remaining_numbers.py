01c79f019ac154155c49c1fb2abded017cb63e79
Batsatsugo <corajianchristian@gmail.com>
2025-03-13 13:50:39 +0800

    pseudo code created

---
+++ ask_user_10_numbers_print_the_result_of_the_first_number_minus_all_of_the_remaining_numbers.py
@@ -0,0 +1,7 @@
+# DECLARE numbers, num, sum_remaining, result
+# FOR i in RANGE (10)
+# num = ('Enter number {i + 1} ')
+# numbers.append(num)
+# sum_remaining = sum(numbers[1:])
+# result = numbers[0] - sum_remaining
+# PRINT("Result:", result)
