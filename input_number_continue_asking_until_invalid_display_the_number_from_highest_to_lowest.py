67259083d4eea807654669e0cae7d8b7f3069768
Batsatsugo <corajianchristian@gmail.com>
2025-03-13 19:37:31 +0800

    while loop and try and except and number append

--- input_number_continue_asking_until_invalid_display_the_number_from_highest_to_lowest.py
+++ input_number_continue_asking_until_invalid_display_the_number_from_highest_to_lowest.py
@@ -1,10 +1,20 @@
 # DECLARE numbers, num
+
+numbers = []
+
 # WHILE TRUE
 # TRY
 # num = ('Enter a number (or anything else to stop): ')
 # numbers.append(num)
 # EXCEPT ValueError
 # BREAK
+while True:
+    try:
+        num = int(input("Enter a number (or anything else to stop): "))
+        numbers.append(num)
+    except ValueError:
+        break
+
 # SORT NUMBERS FROM HIGHEST TO LOWEST
 # numbers.sort(reverse=True)
 # PRINT('Numbers in descending order:', numbers)
