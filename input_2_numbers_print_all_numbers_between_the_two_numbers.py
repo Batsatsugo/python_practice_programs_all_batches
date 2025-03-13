3bae09494f92f8fb5204b9df534578b243c49b3b
Batsatsugo <corajianchristian@gmail.com>
2025-03-13 14:18:52 +0800

    for loop

--- input_2_numbers_print_all_numbers_between_the_two_numbers.py
+++ input_2_numbers_print_all_numbers_between_the_two_numbers.py
@@ -1,5 +1,12 @@
 # DECLARE first_number, second_number, num
+
 # INPUT first_number
+first_number = int(input("Enter the first number: "))
+
 # INPUT second_number
+second_number = int(input("Enter the second number: "))
+
 # FOR num IN RANGE (first_number, second_number + 1)
+for num in range(first_number, second_number + 1):
+
 # PRINT ('num')
