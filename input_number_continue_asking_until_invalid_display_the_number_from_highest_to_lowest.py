c01e0620515fa79d77073f98bfcccdcf013e086e
Batsatsugo <corajianchristian@gmail.com>
2025-03-13 19:36:35 +0800

    pseudo code added

---
+++ input_number_continue_asking_until_invalid_display_the_number_from_highest_to_lowest.py
@@ -0,0 +1,10 @@
+# DECLARE numbers, num
+# WHILE TRUE
+# TRY
+# num = ('Enter a number (or anything else to stop): ')
+# numbers.append(num)
+# EXCEPT ValueError
+# BREAK
+# SORT NUMBERS FROM HIGHEST TO LOWEST
+# numbers.sort(reverse=True)
+# PRINT('Numbers in descending order:', numbers)
