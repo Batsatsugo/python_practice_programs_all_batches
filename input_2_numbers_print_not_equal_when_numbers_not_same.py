1ed5ec589293095538ed593f2257b6eb9fd2e2ea
Batsatsugo <corajianchristian@gmail.com>
2025-03-13 13:18:01 +0800

    if condition and print not equal

--- input_2_numbers_print_not_equal_when_numbers_not_same.py
+++ input_2_numbers_print_not_equal_when_numbers_not_same.py
@@ -1,5 +1,12 @@
 # DECLARE first_number, second_number
+
 # INPUT first_number
+first_number = int(input('Enter first number: '))
+
 # INPUT second_number
+second_number = int(input('Enter first number: '))
+
 # IF A NOT EQUAL TO B
 # PRINT ('NOT EQUAL')
+if first_number != second_number:
+    print('NOT EQUAL')
