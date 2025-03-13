d7b11e3af0ef3b4452402a90ded2bd8f5d87493c
Batsatsugo <corajianchristian@gmail.com>
2025-03-13 12:50:25 +0800

    pseudo code created

---
+++ input_10_numbers_print_how_many_are_odd_numbers.py
@@ -0,0 +1,8 @@
+# SET count to 0
+# FOR i FROM 1 TO 10 DO
+# PRINT Enter a number , i, :
+# INPUT num
+# IF num MOD 2 NOT EQUAL TO 0 THEN
+# COUNT = COUNT + 1
+# ENDIF
+# PRINT number of odd numbers: , count
