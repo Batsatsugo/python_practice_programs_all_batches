264d5320d7053341f76986ed716840e3f116a8bc
Batsatsugo <corajianchristian@gmail.com>
2025-03-13 11:44:13 +0800

    if condition

--- input_2_numbers_print_equal_when_numbers_is_same.py
+++ input_2_numbers_print_equal_when_numbers_is_same.py
@@ -1,5 +1,12 @@
 # DECLARE first_number, second_number
+
 # INPUT first_number
+first_number = int(input('Enter first number: '))
+
 # INPUT second_number
+second_number = int(input('Enter second number: '))
+
 # IF first_number == second_number
 # PRINT ('EQUAL')
+if first_number == second_number:
+    print("EQUAL")
