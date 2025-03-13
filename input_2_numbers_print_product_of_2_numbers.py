3abb503013e3c60243bea6a347bdb07265d5cf2b
Batsatsugo <corajianchristian@gmail.com>
2025-03-13 12:15:28 +0800

    changed sign from '+' to '*'

--- input_2_numbers_print_product_of_2_numbers.py
+++ input_2_numbers_print_product_of_2_numbers.py
@@ -7,7 +7,7 @@ first_number = int(input('Enter first number: '))
 second_number = int(input('Enter second number: '))
 
 # product = first_number + second_number
-product = first_number + second_number
+product = first_number * second_number
 
 # PRINT (The sum of the two numbers is " + product
 print("The sum of the two numbers is " + str(product))
