7dd4bdc92ca907f8eb748c5a0f4797741bc3efd4
Batsatsugo <corajianchristian@gmail.com>
2025-03-13 12:23:58 +0800

    print the result

--- input_2_numbers_print_the_result_when_the_first_number_is_raised_to_the_second_number.py
+++ input_2_numbers_print_the_result_when_the_first_number_is_raised_to_the_second_number.py
@@ -1,5 +1,13 @@
 # DECLARE first_number, second_number, result
+
 # INPUT first_number
+first_number = int(input('Enter first number: '))
+
 # INPUT second_number
+second_number = int(input('Enter second number: '))
+
 # result = first_number ** second_number
+result = first_number ** second_number
+
 # PRINT (The sum of the two numbers is " + result
+print('The result is ' + str(result))
