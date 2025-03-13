e7c11fec5ac07d561bc66b103e4281687bf43dfb
Batsatsugo <corajianchristian@gmail.com>
2025-03-13 13:30:59 +0800

    if condition and print the quotient or error

--- input_2_numbers_print_the_quotient_of_two_numbers_without_the_decimal_point.py
+++ input_2_numbers_print_the_quotient_of_two_numbers_without_the_decimal_point.py
@@ -1,8 +1,19 @@
 # DECLARE first_number, second_number, quotient
+
 # INPUT first_number
+first_number = int(input('Enter first number: '))
+
 # INPUT second_number
+second_number = int(input('Enter first number: '))
+
 # IF second_number not equal to 0
 # quotient = first_number // second_number
 # PRINT ("Quotient: ", quotient)
+if second_number != 0:
+    quotient = first_number // second_number
+    print("Quotient:", quotient)
+
 # ELSE
 # PRINT ("Error: Division by zero is not allowed")
+else:
+    print("Error: Division by zero is not allowed.")
