c72baf745b7ec2ada29d595f683a1652050b7761
Batsatsugo <corajianchristian@gmail.com>
2025-03-13 19:26:55 +0800

    pseudocode added

---
+++ input_number_continue_asking_until_invalid_display_highest_number.py
@@ -0,0 +1,10 @@
+# DECLARE highest, num
+# highest = None
+# WHILE TRUE
+# TRY
+# num = ('Enter a number (or any non-numeric value to stop): ')
+# IF highest IS None or num > highest
+# highest = num
+# BREAK
+# IF highest IS NOT None
+# PRINT('The highest number entered was: ', highest)
