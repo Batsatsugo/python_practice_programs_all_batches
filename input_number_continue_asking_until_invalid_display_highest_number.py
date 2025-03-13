89d6cd9dc31b012a3d25244b74189d37f6f8256f
Batsatsugo <corajianchristian@gmail.com>
2025-03-13 19:27:57 +0800

    while loop and try and except

--- input_number_continue_asking_until_invalid_display_highest_number.py
+++ input_number_continue_asking_until_invalid_display_highest_number.py
@@ -1,4 +1,6 @@
 # DECLARE highest, num
+
+highest = None
 # highest = None
 # WHILE TRUE
 # TRY
@@ -6,5 +8,13 @@
 # IF highest IS None or num > highest
 # highest = num
 # BREAK
+while True:
+    try:
+        num = int(input("Enter a number (or any non-numeric value to stop): "))
+        if highest is None or num > highest:
+            highest = num
+    except ValueError:
+        break
+
 # IF highest IS NOT None
 # PRINT('The highest number entered was: ', highest)
