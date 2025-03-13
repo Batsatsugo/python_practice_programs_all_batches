2ebdc688194a94448e898c2fabced481459d0678
Batsatsugo <corajianchristian@gmail.com>
2025-03-13 12:21:42 +0800

    created file and pseudo code

---
+++ input_2_numbers_print_the_result_when_the_first_number_is_raised_to_the_second_number.py
@@ -0,0 +1,5 @@
+# DECLARE first_number, second_number, result
+# INPUT first_number
+# INPUT second_number
+# result = first_number ** second_number
+# PRINT (The sum of the two numbers is " + result
