2bbf498b60eeeee6d8822ae5bd1b59b2280d722b
Batsatsugo <corajianchristian@gmail.com>
2025-03-13 13:20:33 +0800

    pseudo code created

---
+++ input_2_numbers_print_difference_of_the_two_numbers.py
@@ -0,0 +1,5 @@
+# DECLARE first_number, second_number, difference
+# INPUT first_number
+# INPUT second_number
+# difference = first_number - second_number
+# PRINT (The sum of the two numbers is " + difference)
