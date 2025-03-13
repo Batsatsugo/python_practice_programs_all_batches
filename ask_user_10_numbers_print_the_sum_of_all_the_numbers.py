c4b8699494048acfe0f926b25f58314bf838d974
Batsatsugo <corajianchristian@gmail.com>
2025-03-13 12:40:24 +0800

    print the sum of 10 numbers

--- ask_user_10_numbers_print_the_sum_of_all_the_numbers.py
+++ ask_user_10_numbers_print_the_sum_of_all_the_numbers.py
@@ -1,8 +1,14 @@
 # total = 0
+total = 0
+
 # FOR i FROM 1 TO 10 DO
 # DISPLAY "Enter number ", i, ": "
 # INPUT num
 # total = total + num
+for i in range (10):
+    num = int(input(f'Enter number {i + 1}: '))
+    total += num
 # END FOR
-#
-# DISPLAY "The sum of 10 numbers is: ", total
+
+# PRINT "The sum of 10 numbers is: ", total
+print('The sum of 10 numbers is: ', total)
