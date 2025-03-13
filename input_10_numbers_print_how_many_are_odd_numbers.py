0ec7b2f219584a89f79f703f583b254f9b3a9f77
Batsatsugo <corajianchristian@gmail.com>
2025-03-13 12:52:58 +0800

    print how many are odd numbers

--- input_10_numbers_print_how_many_are_odd_numbers.py
+++ input_10_numbers_print_how_many_are_odd_numbers.py
@@ -1,8 +1,16 @@
 # SET count to 0
+count = 0
+
 # FOR i FROM 1 TO 10 DO
 # PRINT Enter a number , i, :
 # INPUT num
 # IF num MOD 2 NOT EQUAL TO 0 THEN
 # COUNT = COUNT + 1
+for i in range(10):
+    num = int(input(f'Enter a number {i + 1}: '))
+    if num % 2 != 0:
+        count += 1
 # ENDIF
+
 # PRINT number of odd numbers: , count
+print(f'Number of Odd Numbers: {count}')
