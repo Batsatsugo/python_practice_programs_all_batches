c427f159ba1bdd3e22189c5ad4af93b185c97fd1
Batsatsugo <corajianchristian@gmail.com>
2025-03-13 12:05:19 +0800

    pseudo code created

---
+++ input_2_numbers_print_product_of_2_numbers.py
@@ -0,0 +1,5 @@
+# DECLARE first_number, second_number, product
+# INPUT first_number
+# INPUT second_number
+# product = first_number + second_number
+# PRINT (The sum of the two numbers is " + product
