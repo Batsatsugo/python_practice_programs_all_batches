44d82bdd11ed0da28866d6fd71454f40d7a4f1cd
Batsatsugo <corajianchristian@gmail.com>
2025-03-13 13:10:59 +0800

    if condition

--- input_2_numbers_print_smaller_number.py
+++ input_2_numbers_print_smaller_number.py
@@ -1,7 +1,15 @@
 # DECLARE first_number, second_number
+
 # INPUT first_number
+first_number = int(input('Enter first number: '))
+
 # INPUT second_number
+second_number = int(input('Enter first number: '))
+
 # IF first_number < second_number
 # PRINT first_number
+if first_number < second_number:
+    print(first_number)
+    
 # ELSE
 # PRINT second_number
